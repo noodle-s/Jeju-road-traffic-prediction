7c080d8fe3fa0e09ea74daa0cfb8d29d74fb7591
noodle-s <41015614+noodle-s@users.noreply.github.com>
2022-11-05 23:35:56 +0900

    [update] multiple feature add | hyperparamter tuning 진행 중

--- src/oym/model_test.py
+++ src/oym/model_test.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler
 import lightgbm as lgbm
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectFromModel
@@ -13,6 +13,12 @@ import eli5
 from catboost import CatBoostRegressor
 from imblearn.over_sampling import ADASYN
 from bayes_opt import BayesianOptimization
+from sklearn.inspection import permutation_importance
+import optuna
+from optuna import Trial
+from optuna.samplers import TPESampler
+from pyproj import Geod
+
 
 data_path = '../../data/'
 train_file = 'train.csv'
@@ -39,12 +45,17 @@ def bayes_opt(model, random_state=1):
 
 def train_preprocessing():
     data = pd.read_csv(data_path + train_file, encoding='euckr')
+    data = diff_long_lat_add(data)
     data = holiday_add(data)
-    data = road_count_add(data)
+    # data = road_count_add(data)
     data = month_year_add(data)
-    data = month_visitor_add(data)
+    data = time_quarter_add(data)
+    data = meter_sec_add(data)
+    data = distance_add(data)
+    # data = month_visitor_add(data)
 
-    num_col = ['start_latitude', 'start_longitude', 'end_latitude', 'end_longitude', 'road_visit_count','visitor']
+    num_col = ['start_latitude', 'start_longitude', 'end_latitude', 'end_longitude',
+               'diff_longtitude', 'diff_latitude', 'maximum_meter_second', 'maximum_speed_limit']
     cat_col = ['road_name', 'start_node_name', 'end_node_name', 'day_of_week']
     scalers = {}
     encoders = {}
@@ -60,7 +71,7 @@ def train_preprocessing():
         encoders[col] = encoder
 
     target = data['target']
-    data = data.drop(['target','id', 'connect_code', 'multi_linked', 'year', 'vehicle_restricted', 'height_restricted'], axis=1)
+    data = data.drop(['target','id', 'multi_linked', 'connect_code', 'vehicle_restricted', 'height_restricted'], axis=1)
     exist_dict = {'없음': 0, '있음': 1}
     data = data.replace({"start_turn_restricted": exist_dict, 'end_turn_restricted': exist_dict})
     group = ['road_name', 'lane_count', 'start_turn_restricted', 'road_type',
@@ -69,6 +80,12 @@ def train_preprocessing():
     split_data = {"X_train": X_train, "X_val": X_val, "y_train": y_train, "y_val": y_val}
 
     return split_data, encoders, scalers
+
+def diff_long_lat_add(data):
+    data['diff_longtitude'] = data['end_longitude'] - data['start_longitude']
+    data['diff_latitude'] = data['end_latitude'] - data['start_latitude']
+
+    return data
 
 def holiday_add(data):
     krx = ecals.get_calendar("XKRX")
@@ -82,7 +99,6 @@ def holiday_add(data):
     data['holiday'] = data['base_date'].isin(open_date)
     holiday_exist = {True: 0, False: 1}
     data['holiday'] = data['holiday'].map(holiday_exist)
-    data['holiday'].value_counts()
 
     return data
 
@@ -95,15 +111,52 @@ def month_year_add(data):
 
     return data
 
+def time_quarter(data):
+    quarter = 3
+
+    if 6 > data >= 0:
+        quarter = 0
+
+    elif 12 > data >= 6:
+        quarter = 1
+
+    elif 18 > data >= 12:
+        quarter = 2
+
+    return quarter
+
+def time_quarter_add(data):
+    data['time_quarter'] = data['base_hour'].apply(lambda e: time_quarter(e))
+
+    return data
+
+def get_distance(data):
+    g = Geod(ellps='WGS84')
+    # 2D distance in meters with longitude, latitude of the points
+    azimuth1, azimuth2, distance_2d = g.inv(data['start_longitude'], data['start_latitude'], data['end_longitude'],
+                                            data['end_latitude'])
+
+    return distance_2d
+
+def distance_add(data):
+    data['distance'] = data.apply(get_distance,axis=1)
+
+    return data
+
+def meter_sec_add(data):
+    data['maximum_meter_second'] = data['maximum_speed_limit'] / 3.6
+
+    return data
+
 def road_count_add(data):
-    data['base_hour'] = data['base_hour'].astype(str)
-    data['base_date_hour'] = data['base_date'].astype(str) + data['base_hour'].str.zfill(2)
-    data['base_date_hour'] = pd.to_datetime(data['base_date_hour'], format='%Y%m%d%H')
-    road_visit_count = data[["base_date_hour",'road_name']]
-    road_visit_count = road_visit_count.groupby(["base_date_hour","road_name"],as_index=False).value_counts()
+    # data['base_hour'] = data['base_hour'].astype(str)
+    # data['base_date_hour'] = data['base_date'].astype(str) + data['base_hour'].str.zfill(2)
+    # data['base_date_hour'] = pd.to_datetime(data['base_date_hour'], format='%Y%m%d%H')
+    road_visit_count = data[["day_of_week",'road_name','holiday','base_hour','start_node_name','end_node_name']]
+    road_visit_count = road_visit_count.groupby(["day_of_week",'road_name','holiday','base_hour','start_node_name','end_node_name'],as_index=False).value_counts()
     road_visit_count = road_visit_count.rename(columns={"count": "road_visit_count"})
-    data = data.merge(road_visit_count, on=['base_date_hour','road_name'])
-    data = data.drop('base_date_hour', axis=1)
+    data = data.merge(road_visit_count, on=["day_of_week",'road_name','holiday','base_hour','start_node_name','end_node_name'])
+    # data = data.drop('base_date_hour', axis=1)
     data['base_hour'] = data['base_hour'].astype(int)
     data['base_date'] = data['base_date'].astype(int)
 
@@ -161,11 +214,21 @@ def cat_feature_importance(model, x_val):
     plt.show()
     plt.close()
 
-def permutation_importance(model, X_valid, y_valid, features):
-    perm = PermutationImportance(model, random_state=42).fit(X_valid, y_valid)
-    print(eli5.format_as_text(explain_weights.explain_permutation_importance(perm, feature_names=features, top=40)))
+def get_permutation_importance(model, X_valid, y_valid):
+    result = permutation_importance(
+        model, X_valid, y_valid, n_repeats=10, random_state=42
+    )
 
-    explain_weights.explain_permutation_importance(perm, feature_names=features, top=47)
+    sorted_importances_idx = result.importances_mean.argsort()
+    importances = pd.DataFrame(
+        result.importances[sorted_importances_idx].T,
+        columns=X_valid.columns[sorted_importances_idx],
+    )
+    ax = importances.plot.box(vert=False, whis=10)
+    ax.set_title("Permutation Importances (valid set)")
+    ax.axvline(x=0, color="k", linestyle="--")
+    ax.set_xlabel("Decrease in accuracy score")
+    ax.figure.tight_layout()
 
 
 def lgbm_fit(data):
@@ -184,36 +247,62 @@ def lgbm_fit(data):
     lgbm.early_stopping(stopping_rounds=100)
     model.fit(data["X_train"], data["y_train"], eval_metric='MAE', eval_set=evals,
               callbacks = [lgbm.record_evaluation(callback)])
-    permutation_importance(model, data['X_val'], data["y_val"], list(data['X_val'].columns))
+    get_permutation_importance(model, data['X_val'], data["y_val"])
     lgbm_loss_plot(callback)
     lgbm_feature_importance(model)
 
     return model
 
-def cat_boost_fit(data, learning_rate, min_child_samples, max_depth):
+def cat_boost_fit(trial):
+    data, encoders, scalers = train_preprocessing()
+
+    cbrm_param = {
+        'iterations': 200000,
+        'early_stopping_rounds': 100,
+        'eval_metric': 'MAE',
+        'learning_rate': trial.suggest_uniform('learning_rate', 0.01, 0.1),
+        'reg_lambda': trial.suggest_uniform('reg_lambda', 1e-5, 100),
+        'depth': trial.suggest_int('depth', 1, 16),
+        'min_data_in_leaf': trial.suggest_int('min_data_in_leaf', 1, 30),
+        'leaf_estimation_iterations': trial.suggest_int('leaf_estimation_iterations', 1, 15),
+        'bagging_temperature': trial.suggest_loguniform('bagging_temperature', 0.01, 100.00),
+        'devices':'0:3',
+        'task_type':'GPU',
+        'random_state' : 0
+    }
+
     evals = [(data['X_val'], data["y_val"])]
-    model = CatBoostRegressor(iterations = 250000,
-                              early_stopping_rounds=100,
-                               learning_rate=learning_rate,
-                               min_child_samples=int(min_child_samples), # 150, 200
-                               max_depth=int(max_depth),
-                               eval_metric='MAE',
-                               task_type="GPU",
-                               devices='0:1') #
+    # model = CatBoostRegressor(iterations = 200000,
+    #                           early_stopping_rounds=100,
+    #                            learning_rate=0.018,
+    #                            min_data_in_leaf=21, # 100
+    #                            max_depth=11,
+    #                            eval_metric='MAE',
+    #                            task_type="GPU",
+    #                            devices='2:3',
+    #                            reg_lambda= 14.721,
+    #                           leaf_estimation_iterations=9,
+    #                           bagging_temperature= 0.0226
+    #                           ) #
+    model = CatBoostRegressor(**cbrm_param)
     model.fit(data["X_train"], data["y_train"], eval_set=evals, verbose=True)
-    cat_loss_plot(model.evals_result_['learn']['MAE'])
-    # permutation_importance(model, data['X_val'], data["y_val"], list(data['X_val'].columns))
+    # cat_loss_plot(model.evals_result_['learn']['MAE'])
+    # get_permutation_importance(model, data['X_val'], data["y_val"])
     # cat_feature_importance(model, data['X_val'])
 
     # return model
-    return 5 - model.evals_result_['learn']['MAE'][-1]
+    return model.evals_result_['validation']['MAE'][-1]
 
 def test_preprocessing(encoders, scalers):
     data = pd.read_csv(data_path + test_file, encoding='euckr')
+    data = diff_long_lat_add(data)
     data = holiday_add(data)
+    # data = road_count_add(data)
     data = month_year_add(data)
-    data = road_count_add(data)
-    data = month_visitor_add(data)
+    data = time_quarter_add(data)
+    data = meter_sec_add(data)
+    data = distance_add(data)
+    # data = month_visitor_add(data)
     exist_dict = {'없음': 0, '있음': 1}
     data = data.replace({"start_turn_restricted": exist_dict, 'end_turn_restricted': exist_dict})
 
@@ -222,11 +311,8 @@ def test_preprocessing(encoders, scalers):
 
     for col in list(encoders.keys()):
         data[col] = encoders[col].transform(data[col])
-    # data = data.drop(['id', 'year', 'multi_linked', 'connect_code',
-    #                   'vehicle_restricted', 'height_restricted', 'start_node_name',
-    #                   'start_node_name','start_latitude','start_longitude','end_node_name',
-    #                   'end_latitude','end_longitude'], axis=1)
-    data = data.drop(['id', 'multi_linked', 'connect_code'], axis=1)
+
+    data = data.drop(['id', 'multi_linked', 'connect_code', 'vehicle_restricted', 'height_restricted'], axis=1)
 
     return data
 
@@ -237,10 +323,17 @@ def predict_to_csv(model, test_data):
     submis.to_csv(data_path + 'res_test.csv', index=False)
 
 def main():
-    train_data, encoders, scalers = train_preprocessing()
-    model = partial(cat_boost_fit, train_data)
-    bayes_opt(model)
-    # model = cat_boost_fit(train_data)
+    # train_data, encoders, scalers = train_preprocessing()
+    # model = partial(cat_boost_fit, train_data)
+    # bayes_opt(model)
+    # model = cat_boost_fit()
+    sampler = TPESampler(seed=10)
+    optuna_cbrm = optuna.create_study(direction='minimize', sampler=sampler)
+    optuna_cbrm.optimize(cat_boost_fit, n_trials=50)
+
+    cbrm_trial = optuna_cbrm.best_trial
+    cbrm_trial_params = cbrm_trial.params
+    print('Best Trial: score {},\nparams {}'.format(cbrm_trial.value, cbrm_trial_params))
     # model = lgbm_fit(train_data)
 
     # test_data = test_preprocessing(encoders, scalers)
